ba87816ff9d7bef90528a23b75c3d809228495c3
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-10 10:09:01 +0100

    generate a token instead of harcoding it

--- complete/src/integration-test/groovy/demo/CurrentUserByJwtTenantResolverSpec.groovy
+++ complete/src/integration-test/groovy/demo/CurrentUserByJwtTenantResolverSpec.groovy
@@ -1,9 +1,12 @@
 package demo
 
+import grails.plugin.springsecurity.rest.token.AccessToken
+import grails.plugin.springsecurity.rest.token.generation.TokenGenerator
 import grails.testing.mixin.integration.Integration
 import org.grails.datastore.mapping.multitenancy.exceptions.TenantNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletWebRequest
 import spock.lang.Specification
@@ -15,6 +18,8 @@ class CurrentUserByJwtTenantResolverSpec extends Specification {
 
     @Autowired
     CurrentUserByJwtTenantResolver currentUserTenantResolver
+
+    TokenGenerator tokenGenerator
 
     void "Test HttpHeader resolver throws an exception outside a web request"() {
         when:
@@ -46,19 +51,15 @@ class CurrentUserByJwtTenantResolverSpec extends Specification {
 
         setup:
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "/foo")
-        String jwt = '''\
-eyJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOiJINHNJQUFBQUFBQUFBSlZTdjA4VVFSUitleHhDZ2xF\
-d2tjUUNHN0V6ZXdtV1Z3RkJBcGtjeHZNc01OSE03VDZXZ2RtWmRXYjJ1R3ZJVlZKUVFGUVNFbHBMXC9oT\
-nRcL0FPTUZMVFV0THhaT1BhMElVNjFlZlB0OSt2TjZRV01XZ012RThPRnRHRW04MFNvMEdaR3FNUmlsQn\
-ZoZW1GdTBjVG9Dc1J5QVd6UkJLNVBVSUdBUVVYRURoNnhMZDdoTmNsVlVsdHJiMkhrNmwwRGM5b2tONHd\
-iaHFlNG84MTJlTXNkYVlOXC9DWlRVd2ZjS2pLM0RGSThpblN2WDBHcXBtd21EOFRwTWxqT21vMjBcL2Vo\
-elJEU29udUxURDBERlV2QzB4WmpEQmM3ZXBTVldnZGZEdzJtenVoS3cxMGRVWmpHZmNXbkwzVDVLbTg5Yj\
-l2YmVwS01FbjJJVnFOd3ZvVUhmUFBUVDBQT0dpbHBKU0M2M3NiRXVsT2hZYndvc1RmM1wvbXk2K0RrMzZy\
-QWtDZHZMajduM0wrWkFINlB6NWNQaTJLRGlJSDAwUFdTMWk5bTVHYnFaTDVyVUd2XC9QdjQ5ZGVqaTczM0\
-k2VHNFYVwvK2Z4K3o4emZOOVJaMW1uSERuUjdhRWRIdVZQMDNrU1wvY1RUN1lRaTlzaWpTVFNDOUtPWXh2\
-SlVwaWlsczFXZzc2ZG5EXC96UnBiK3ZodWhiSDVsWVlmM090UWRtMUkrRUdSMnk4c1pKcld0WDkrK1BQZ\
-zJSOGlXWVhSRHBjNVV1MlRKYWlScDIwMG4wK1BaaWErbmUwWElRWVArZ3JPZUdRVkZBTUFBQT09Iiwic3\
-ViIjoidmVjdG9yIiwicm9sZXMiOlsiUk9MRV9WSUxMQUlOIl0sImlhdCI6MTUwNDc4MTEyNX0.cf5rGNrNolchQ3QyMsPB544fwzYGiihBkRF8KU6soxc'''
+        def userDetails = Stub(UserDetails) {
+            getUsername() >> 'vector'
+            isAccountNonExpired() >> true
+            isAccountNonLocked() >> true
+            isCredentialsNonExpired() >> true
+            isEnabled() >> true
+        }
+        AccessToken accessToken = tokenGenerator.generateAccessToken(userDetails, 3600)
+        String jwt = accessToken.accessToken
 
         request.addHeader('Authorization', "Bearer $jwt")
         RequestContextHolder.setRequestAttributes(new ServletWebRequest(request))
@@ -74,4 +75,3 @@ ViIjoidmVjdG9yIiwicm9sZXMiOlsiUk9MRV9WSUxMQUlOIl0sImlhdCI6MTUwNDc4MTEyNX0.cf5rGN
         RequestContextHolder.setRequestAttributes(null)
     }
 }
-
